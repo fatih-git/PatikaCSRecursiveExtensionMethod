ea68657b374eef705af4f4bdccd03bc1925a7fa5
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-12-07 16:58:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Deneme2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 123;
+            bool sonuc = x.IsEventNumber();
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine(faktoriyel(7));
+
+        }
+
+        public static int faktoriyel(int sayi)
+        {
+            if(sayi == 0 || sayi==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return sayi*faktoriyel(sayi-1);
+            }
+        }
+
+    }
+
+    public static class MyExtensionClass
+    {
+        public static bool IsEventNumber(this int value)
+        {
+            if (value % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+}
